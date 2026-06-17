d3f1b8d3b51e1ce082b64b170b2b05476c145c1f
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-06-17 17:26:11 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,21 @@ import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
+    // Cabeçalho fixo
     <header className="bg-white bg-opacity-100 w-full z-50">
+      {/* Navegação principal */}
       <nav id= "header"className="mx-auto flex max-w-7xl items-center p-6 lg:px-8">
+        {/* Área da logo */}
         <div className="logo-container">
           <img src={logo} alt="KDrop Hub" className="logo-img" />
-
+          
+          {/* Nome */}
           <span className="logo-text">
             KDrop<span>Hub</span>
           </span>
         </div>
-
+        
+        {/* Links */}
         <div className="others">
           <a href="#beneficios">Benefícios</a>
           <a href="#pilares">Ícones</a>
