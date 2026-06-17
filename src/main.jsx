8982371c715de98081ac78394582219bc7293e3f
Adriel Wanderlind <adrielwanderlind80@gmail.com>
2026-06-17 17:23:45 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+// Importa React
 import React from "react";
+// Responsável por renderizar componentes React no navegador
 import ReactDOM from "react-dom/client";
+// Importa o componente principal da aplicação
 import App from "./App";
 
+// Procura o elemento com id="root" no index.html 
+// e monta toda aplicação dentro dele
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
